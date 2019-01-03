ff3d8e9f06a31e6c8596aff5f957af85985f743d
Agustin Aguilar <agustin@ripiocredit.network>
2019-01-03 22:36:14 +0100

    PEP8 Auto fix

--- marmopy/utils.py
+++ marmopy/utils.py
@@ -1,39 +1,45 @@
 from eth_utils import *
-from coincurve.keys import PrivateKey,PublicKey
+from coincurve.keys import PrivateKey, PublicKey
 from Crypto.Hash import keccak
 
-class Credentials: 
-    def __init__(self,private_key):
+
+class Credentials:
+    def __init__(self, private_key):
         self.private_key = PrivateKey(bytes(bytearray.fromhex(private_key)))
-        
+
     def getAddress(self):
         keccak_hash = keccak.new(digest_bits=256)
-        keccak_hash.update(self.private_key.public_key.format(compressed=False,)[1:])
+        keccak_hash.update(
+            self.private_key.public_key.format(
+                compressed=False,)[
+                1:])
         return to_checksum_address(keccak_hash.digest()[-20:])
-    
-    def signHash(self,hashstring):
+
+    def signHash(self, hashstring):
         msg_hash = bytes(bytearray.fromhex(remove_0x_prefix(hashstring)))
-        
-        signature_bytes = self.private_key.sign_recoverable(msg_hash,hasher=None)
-        
+
+        signature_bytes = self.private_key.sign_recoverable(
+            msg_hash, hasher=None)
+
         assert len(signature_bytes) == 65
-        
-        r =  "0x"+format(big_endian_to_int(signature_bytes[0:32]),"x")
-       
-        s =  "0x"+format(big_endian_to_int(signature_bytes[32:64]),"x")
-        
+
+        r = "0x" + format(big_endian_to_int(signature_bytes[0:32]), "x")
+
+        s = "0x" + format(big_endian_to_int(signature_bytes[32:64]), "x")
+
         v = hex(ord(signature_bytes[64:65]) + 27)
-  
-        return {"r":r,"s":s,"v":v}
-    
+
+        return {"r": r, "s": s, "v": v}
+
+
 def keccak256(hexstring):
     hexstring = remove_0x_prefix(hexstring)
     keccak_hash = keccak.new(digest_bits=256)
     keccak_hash.update(bytearray.fromhex(hexstring))
     return keccak_hash.hexdigest()
-    
-    
-def toHexStringNoPrefixZeroPadded(value,size=64):
-    hexstring = hex(value).replace("0x","")
+
+
+def toHexStringNoPrefixZeroPadded(value, size=64):
+    hexstring = hex(value).replace("0x", "")
     padding_size = 64 - len(hexstring)
-    return "0"*padding_size + hexstring
+    return "0" * padding_size + hexstring
--- tests/IntentActionTest.py
+++ tests/IntentActionTest.py
@@ -4,39 +4,38 @@ from examples import ERC20
 
 
 class IntentTests(unittest.TestCase):
-    
+
     def testInvalidDataTypes(self):
         class ContractTest:
             @IntentAction
-            def balanceOf(self, who='str') : return 'uint256'
-        
+            def balanceOf(self, who='str'): return 'uint256'
+
         contract = ContractTest()
         address = "0x7F5EB5bB5cF88cfcEe9613368636f458800e62CB"
-        
+
         with self.assertRaises(TypeError):
             contract.balanceOf(address)
-            
-            
+
     def testInvalidArgumentsTypes(self):
         class ContractTest:
             @IntentAction
-            def balanceOf(self, who='address') : return 'uint256'
-        
+            def balanceOf(self, who='address'): return 'uint256'
+
         contract = ContractTest()
         address = 123
-        
+
         with self.assertRaises(TypeError):
             contract.balanceOf(address)
 
     def testInvalidSyntax(self):
-        
-         with self.assertRaises(TypeError):
-                
+
+        with self.assertRaises(TypeError):
+
             class ContractTest:
                 @IntentAction
                 def balanceOf(self, who): pass
-            
-      
+
+
 if __name__ == '__main__':
-    
+
     unittest.main()
