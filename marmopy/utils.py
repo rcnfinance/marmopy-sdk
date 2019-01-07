130602a8da6be6cdda6df179b9b2da3036c9b8ca
Emanuel Borda <emanuel@bitpagos.co>
2019-01-07 14:51:08 -0300

    change all pep8

--- marmopy/utils.py
+++ marmopy/utils.py
@@ -2,27 +2,29 @@ from eth_utils.hexadecimal import *
 from eth_hash.auto import keccak as keccak_256
 from web3.auto import w3
 
-class Credentials: 
-    def __init__(self,privateKey):
+
+class Credentials(object):
+    def __init__(self, privateKey):
         self._Account = w3.eth.account.privateKeyToAccount(privateKey)
-        
+
     def getAddress(self):
         return self._Account.address
 
-    def signHash(self,hashstring):
+    def signHash(self, hashstring):
         signature = dict(self._Account.signHash(hashstring))
-  
+
         for key in tuple(signature.keys()):
-            if key in ["r","s","v"]:
+            if key in ["r", "s", "v"]:
                 signature[key] = hex(signature[key])
             else:
                 signature.pop(key)
         return signature
-    
-def keccak256(hexstring: str):
+
+
+def keccak256(hexstring):
     return keccak_256(bytearray.fromhex(remove_0x_prefix(hexstring))).hex()
 
-def toHexStringNoPrefixZeroPadded(value,size=64):
-    hexstring = hex(value).replace("0x","")
+def toHexStringNoPrefixZeroPadded(value, size=64):
+    hexstring = hex(value).replace("0x", "")
     padding_size = 64 - len(hexstring)
-    return "0"*padding_size + hexstring
+    return "0" * padding_size + hexstring
