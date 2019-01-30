4cf997a84c9f9756cb92e6f79469e9458dabffc5
Agustin <agusxrun@gmail.com>
2019-01-30 23:34:00 +0100

    Replace IntentGeneric with Contract class, using IntentAction

--- tests/unit_test/test_intent_generic.py
+++ tests/unit_test/test_intent_generic.py
@@ -1,6 +1,5 @@
 import unittest
-from marmopy import ERC20, IntentGeneric, Wallet, Conf, Intent
-from examples.generic_contract import Contract
+from marmopy import Contract, ERC20, Wallet, Conf, Intent
 
 
 class IntentGenericTests(unittest.TestCase):
@@ -16,18 +15,16 @@ class IntentGenericTests(unittest.TestCase):
         [{"constant":true,"inputs":[],"name":"name","outputs":[{"name":"","type":"string"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_spender","type":"address"},{"name":"_value","type":"uint256"}],"name":"approve","outputs":[{"name":"success","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_from","type":"address"},{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transferFrom","outputs":[{"name":"success","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"decimals","outputs":[{"name":"","type":"uint8"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"version","outputs":[{"name":"","type":"string"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_spender","type":"address"},{"name":"_subtractedValue","type":"uint256"}],"name":"decreaseApproval","outputs":[{"name":"success","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"_owner","type":"address"}],"name":"balanceOf","outputs":[{"name":"balance","type":"uint256"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"PRICE","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"symbol","outputs":[{"name":"","type":"string"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transfer","outputs":[{"name":"success","type":"bool"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_spender","type":"address"},{"name":"_addedValue","type":"uint256"}],"name":"increaseApproval","outputs":[{"name":"success","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"_owner","type":"address"},{"name":"_spender","type":"address"}],"name":"allowance","outputs":[{"name":"remaining","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"beneficiary","type":"address"}],"name":"buyTokens","outputs":[],"payable":true,"type":"function"},{"inputs":[],"payable":false,"type":"constructor"},{"payable":true,"type":"fallback"},{"anonymous":false,"inputs":[{"indexed":true,"name":"to","type":"address"},{"indexed":false,"name":"amount","type":"uint256"}],"name":"Mint","type":"event"},{"anonymous":false,"inputs":[],"name":"CreatedToken","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"_from","type":"address"},{"indexed":true,"name":"_to","type":"address"},{"indexed":false,"name":"_value","type":"uint256"}],"name":"Transfer","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"_owner","type":"address"},{"indexed":true,"name":"_spender","type":"address"},{"indexed":false,"name":"_value","type":"uint256"}],"name":"Approval","type":"event"}]
         """
 
-        self.token_ontract_address = "0x6B0F919A5d450Fa5e6283Ff6178dC1FCd195FD2A"
+        self.token_address = "0x6B0F919A5d450Fa5e6283Ff6178dC1FCd195FD2A"
         self.wallet = Wallet("0x5ef1dbf8ef171b33cd72a5d11b713442dcd2c70695753a0f6df9b38136e08d54", self.conf)
 
-        self.contract = Contract(abi)
+        self.ERC20 = Contract(abi)
 
     def test_transfer(self):
-        data = self.contract.transfer({"_to": "0x009ab4de1234c7066197d6ed75743add3576591f", "_value": 4})
+        intent_action = self.ERC20(self.token_address).transfer({"_to": "0x009ab4de1234c7066197d6ed75743add3576591f", "_value": 4})
 
-        intent = IntentGeneric(
-            data=data,
-            contract_address=self.token_ontract_address,
-            value=0,
+        intent = Intent(
+            intent_action=intent_action,
             expiration=1548030494,
             max_gas_price=10 ** 32
         )
@@ -42,12 +39,10 @@ class IntentGenericTests(unittest.TestCase):
             expiration = 10 ** 32
         ))
 
-        data = self.contract.transfer({"_to": "0x009ab4de1234c7066197d6ed75743add3576591f", "_value": 100 * 10 ** 18})
+        intent_action = self.ERC20(self.token_address).transfer({"_to": "0x009ab4de1234c7066197d6ed75743add3576591f", "_value": 100 * 10 ** 18})
 
-        intent = IntentGeneric(
-            data=data,
-            contract_address=self.token_ontract_address,
-            value=0,
+        intent = Intent(
+            intent_action=intent_action,
             expiration=10 ** 36
         )
 
