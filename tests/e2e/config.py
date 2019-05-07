4a76bf8ab21df3daa5c23f8ed612b769c29335e6
Agustin Aguilar <Agusxrun@gmail.com>
2019-05-07 18:56:16 +0200

    Allow custom E2E relayer

--- tests/e2e/config.py
+++ tests/e2e/config.py
@@ -1,10 +1,14 @@
 
 from marmopy import Action
+import os
 
 ETH_NODE = "https://ropsten.node.rcn.loans:8545/"
 RELAYER = "https://marmo-relayer-ropsten.rcn.loans/"
 TEST_ERC20 = "0x2f45b6fb2f28a73f110400386da31044b2e953d4"
 TEST_CONTRACT = "0x1b1c4DC3102abEBE4c469ABA74cc94C381C62010"
+
+if "MARMOPY_E2E_RELAYER" in os.environ:
+    RELAYER = os.environ['MARMOPY_E2E_RELAYER']
 
 class CallTestContract:
     def __init__(self, address):
